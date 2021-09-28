e7c26bfd0c85ec97e959a703debd598784d493f6
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-09-28 23:28:09 +0200

    Added basic styling to mobile view

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import { API } from './utils';
 import VenueList from './VenueList';
 const App = () => {
-	const [longitude, setLongitute] = useState('');
-	const [latitude, setLatitute] = useState('');
+  const [lat, setLatitute] = useState('');
+	const [lng, setLongitute] = useState('');
 	const [venues, setVenues] = useState([]);
 
 	const onFormSubmit = e => {
 		e.preventDefault();
-		fetch(API(40.74224, -73.99386))
+		fetch(API(lat, lng))
 			.then(res => res.json())
 			.then(data => setVenues(data.response.venues))
 			.catch(error => console.log(error));
@@ -16,14 +16,34 @@ const App = () => {
 
 	return (
 		<div className="App">
-			<form onSubmit={onFormSubmit}>
-				<label>Longitude</label>
-				<input type="text" value={longitude} onChange={e => setLongitute(e.target.value)} />
-				<label>Latitude</label>
-				<input type="text" value={latitude} onChange={e => setLatitute(e.target.value)} />
-				<button>Submit</button>
+			<h1 className="heading">Find your venue!</h1>
+			<p className="description">Enter the coordinates and we will give you suggestions!</p>
+			<form className="form" onSubmit={onFormSubmit}>
+				<label className="label">
+					Latitude:
+					<input
+						className="input"
+						type="text"
+						value={lat}
+						onChange={e => setLatitute(e.target.value)}
+						required
+					/>
+				</label>
+
+				<label className="label">
+					Longitude:
+					<input
+						className="input"
+						type="text"
+						value={lng}
+						onChange={e => setLongitute(e.target.value)}
+						required
+					/>
+				</label>
+
+				<button className="submit">Search</button>
 			</form>
-			<VenueList venues={venues} coordinates={{ lat: latitude, lng: longitude }} />
+			{venues && <VenueList venues={venues} coordinates={{ lat, lng }} />}
 		</div>
 	);
 };
--- src/VenueList.js
+++ src/VenueList.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import {distanceCalculator} from './utils'
+import { distanceCalculator } from './utils';
 
+const VenueList = ({ venues, coordinates }) => {
+	const calculateDistance = venue => {
+		const distance = distanceCalculator(
+			venue.location.lat,
+			venue.location.lng,
+			coordinates.lat,
+			coordinates.lng
+		);
+		return { ...venue, distance };
+	};
 
-const VenueList = ({venues, coordinates}) => {
-  const calculateDistance = (venue) => {
-    const distance = distanceCalculator(venue.location.lat, venue.location.lng, 59.3257, 18.0719)
-    return {...venue, distance}
-  }
+	const sortedByDistance = venues.map(calculateDistance).sort((a, b) => a.distance - b.distance);
 
-const sortedByDistance = venues.map(calculateDistance).sort((a, b) => a.distance - b.distance)
-console.log(sortedByDistance)
-
-  return (
-    <div className="venues">
-      {venues &&
-					sortedByDistance.map(venue => {
-						return <p>{venue.name}</p>;
-					})}
-    </div>
-  );
-}
+	return (
+		<div className="venues">
+      <h2>Venues</h2>
+			{venues &&
+				sortedByDistance.map(venue => {
+					return (
+						<div>
+							<p>{venue.name}</p>
+							<p>{venue.distance}m</p>
+						</div>
+					);
+				})}
+		</div>
+	);
+};
 
 export default VenueList;
